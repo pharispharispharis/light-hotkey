39499018b57573c9ec3017dbd6b5f36bedede013
PharisMods <rmassey3234@gmail.com>
2023-07-09 02:07:11 -0500

    Add packager

---
+++ Scripts/Pharis/LightHotkey/packager.bat
@@ -0,0 +1,3 @@
+7z a PharisLightHotkey.7z Scripts PharisLightHotkey.omwscripts changelog.md README.md
+7z rn PharisLightHotkey.7z changelog.md Docs/Pharis/LightHotkey/changelog.md
+7z rn PharisLightHotkey.7z README.md Docs/Pharis/LightHotkey/README.md
